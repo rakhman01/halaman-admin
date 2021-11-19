a6cbb463e78a615912a80f8702edc238a1cd2755
rakhman01 <rakhman031@gmail.com>
2021-11-19 12:33:30 +0700

    first commit

---
+++ src/Api/Routing/Routing.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Router, Switch, Link } from "react-router-dom";
+
+function Routing() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/"></Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default Routing;
